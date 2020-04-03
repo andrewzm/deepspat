f775596bc1b016af8b9bfe209d6f745c402c9d81
quanvu17 <dmqv953@uowmail.edu.au>
2020-04-03 18:26:35 +1100

    Fix covariance function

--- R/cov_fns.R
+++ R/cov_fns.R
@@ -15,13 +15,17 @@
 
 ## Covariance matrix of the weights at the top layer
 cov_exp_tf <- function(x1, x2 = x1, sigma2f, alpha) {
-
+  
   d <- ncol(x1)
   n1 <- nrow(x1)
   n2 <- nrow(x2)
   square_mat <- tf$cast(tf$math$equal(n1,n2), "float32")
-  Dsquared <- tf$constant(matrix(0, n1, n2), name = 'D', dtype = tf$float32)
-    
+  Reg <- Dsquared <- tf$constant(matrix(0, n1, n2), 
+                                 name = 'D', 
+                                 dtype = tf$float32)
+  Reg <- tf$linalg$set_diag(Reg, rep(1e-30, min(n1, n2)))  
+  Reg <- tf$multiply(square_mat, Reg)
+  
   for(i in 1:d) {
     x1i <- x1[, i, drop = FALSE]
     x2i <- x2[, i, drop = FALSE]
@@ -30,21 +34,21 @@ cov_exp_tf <- function(x1, x2 = x1, sigma2f, alpha) {
     alphasep2 <- tf$square(alphasep)
     Dsquared <- tf$add(Dsquared, alphasep2)
   }
-
-  Dsquared <- Dsquared + tf$multiply(square_mat, tf$multiply(1e-30, tf$eye(n1)))
+  
+  Dsquared <- Dsquared + Reg
   D <- tf$sqrt(Dsquared)
   K <- tf$multiply(sigma2f, tf$exp(-0.5 * D))
-    
- return(K)
+  
+  return(K)
 }
 
 cov_sqexp_tf <- function(x1, x2 = x1, sigma2f, alpha) {
-
+  
   d <- ncol(x1)
   n1 <- nrow(x1)
   n2 <- nrow(x2)
   D <- tf$constant(matrix(0, n1, n2), name='D', dtype = tf$float32)
-
+  
   for(i in 1:d) {
     x1i <- x1[, i, drop = FALSE]
     x2i <- x2[, i, drop = FALSE]
